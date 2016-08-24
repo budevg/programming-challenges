5107dc83e27962b68fb02503aaf4fee8e5cbd640
Evgeny Budilovsky <evgeny@e8storage.com>
2016-08-24 23:54:58 +0300

    solve monad

--- by-lang/haskell/nicta-course/check.sh
+++ by-lang/haskell/nicta-course/check.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-stack exec -- doctest -isrc -Wall -fno-warn-type-defaults src/Course/$1.hs
+stack exec \
+      -- doctest -isrc \
+      -Wall -fno-warn-unused-binds -fno-warn-unused-do-bind \
+      -fno-warn-unused-imports -fno-warn-type-defaults \
+      -ferror-spans -fno-warn-type-defaults \
+      src/Course/$1.hs
