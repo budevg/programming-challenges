f57830fc3d6555ad37b177f9eb774293998651a2
Evgeny Budilovsky <budevg@gmail.com>
2016-11-11 10:35:04 +0200

    fix warnings 2

--- by-lang/haskell/nicta-course/check.sh
+++ by-lang/haskell/nicta-course/check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-stack exec -- doctest -isrc -Wall -fno-warn-type-defaults src/Course/$1.hs
+stack exec -- doctest -isrc -Wall -fno-warn-unused-top-binds -fno-warn-orphans -fno-warn-type-defaults src/Course/$1.hs
